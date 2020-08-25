da0ba8c5cfd670adf350d51939f4e718cb384f13
sszxc <im.zhangxc@foxmail.com>
2020-08-25 16:20:06 +0800

    init

---
+++ TagPreProcessing.py
@@ -0,0 +1,31 @@
+# Author: Xuechao Zhang
+# Date: Aug 25th, 2020
+# Description: Apriltag 图片缩放处理
+
+import os
+import sys
+import cv2
+import random
+
+if __name__ == "__main__":
+    print("")
+    print("Python.version: "+sys.version)
+    print("OpenCV.version: "+cv2.__version__)
+
+    print(random.randint(0, 9))
+
+    path = "tag36h11"
+    random_filename = random.choice([x for x in os.listdir(path)\
+        if (os.path.splitext(x)[0][:3] == "tag" and \
+            os.path.splitext(x)[-1][1:] == "png")])  # 选择开头为tag的png图片
+    print(path + "/" + random_filename)
+    img_raw = cv2.imread(path + "/" + random_filename)
+    sp = img_raw.shape # 看看大小
+    print("img_size:" + str(sp))
+    multiple = 20
+    img_resize = cv2.resize(img_raw, (sp[1] * multiple, sp[0] * multiple), interpolation=cv2.INTER_NEAREST)
+        
+    cv2.imshow('image', img_resize)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ apriltag.py
@@ -0,0 +1,40 @@
+# Author: Xuechao Zhang
+# Date: Aug 25th, 2020
+# Description: Apriltag 识别复现
+
+import os
+import sys
+import cv2
+import random
+import datetime
+import numpy as np
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    print("")
+    print("Python.version: "+sys.version)
+    print("OpenCV.version: "+cv2.__version__)
+
+    print(random.randint(0, 9))
+
+    path = "test"
+    random_filename = random.choice([x for x in os.listdir(path)])
+    print(path + "/" + random_filename)
+    img_raw = cv2.imread(path + "/" + random_filename)
+    sp = img_raw.shape  # 看看大小
+    print("img_size:" + str(sp))
+    cv2.imshow('image', img_raw)
+
+    # # 计时开始
+    # starttime = datetime.datetime.now()
+    # print('**********Start!**********')
+
+    # #计时结束
+    # print('**********Stop!**********')
+    # endtime = datetime.datetime.now()
+    # print('Elapsed time:', end='')
+    # print(endtime - starttime)
+    # print('')
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
