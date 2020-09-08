a21f85a64a15ed95983f16ee94d5f8533c4abdd5
sszxc <im.zhangxc@foxmail.com>
2020-09-08 23:29:38 +0800

    矩形提取

--- apriltag.py
+++ apriltag.py
@@ -10,47 +10,48 @@ import datetime
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-def detect(frame):
+def detect(frame, debug = False):
     """
     随意拼凑一下 Aug 31st, 2020
     """
-    cv2.imshow('frame', frame)
+    gray = np.array(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY))    
 
-    gray = np.array(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY))
-    cv2.imshow('gray', gray)
-    """
-    1 blur
-    """
     blur = cv2.GaussianBlur(gray, (3, 3), 0.8)
-    cv2.imshow('blur', blur)
-    """
-    2 adaptive thresholding or  canny
-    """
-    canny = cv2.Canny(blur, 50, 350, apertureSize=3)
-    cv2.imshow('canny', canny)
-    """
-    3 find contours
-    """
+
+    canny = cv2.Canny(blur, 50, 350, apertureSize=3)  # adaptive thresholding or canny
+
+    if debug:
+        cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
+        cv2.imshow('blur', blur)
+        cv2.imshow('canny', canny)
+
     _, contours, hierarchy = cv2.findContours(canny, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    # findContours函数
+    # 参数：二值图；检索模式；轮廓的近似表示方法
+    # contours是个list，其中每个元素都是图像中的一个轮廓，用numpy中的ndarray表示
+    # hierarchy表示contours之间的关系
+    # https://blog.csdn.net/hjxu2016/article/details/77833336
+
 
     """
-    4 compute convex hulls and find maximum inscribed quadrilaterals
+    compute convex hulls and find maximum inscribed quadrilaterals
+    计算凸包 以及 最大内切四边形 还没看懂
     """
     quads = []  # array of quad including four peak points
     hulls = []
     for i in range(len(contours)):
-        if (hierarchy[0, i, 3] < 0 and contours[i].shape[0] >= 4):
-            area = cv2.contourArea(contours[i])
+        if (hierarchy[0, i, 3] < 0 and contours[i].shape[0] >= 4):  # 第i个轮廓不存在父轮廓？ 且其中的点多于4个
+            area = cv2.contourArea(contours[i]) # 计算面积
             if area > 400:
-                hull = cv2.convexHull(contours[i])
-                if (area / cv2.contourArea(hull) > 0.8):
+                hull = cv2.convexHull(contours[i]) # 计算凸包
+                if (area / cv2.contourArea(hull) > 0.8): # 要求不凹太多
                     hulls.append(hull)
-                    quad = cv2.approxPolyDP(hull, 8, True)  # maximum_area_inscribed
+                    quad = cv2.approxPolyDP(hull, 8, True)  # 多边形拟合 平滑处理 (# maximum_area_inscribed)
                     if (len(quad) == 4):
                         areaqued = cv2.contourArea(quad)
                         areahull = cv2.contourArea(hull)
-                        if areaqued / areahull > 0.8 and areahull >= areaqued:
+                        if areaqued / areahull > 0.8 and areahull >= areaqued: # 要求拟合后的多边形面积比拟合前小一点点？
                             quads.append(quad)
 
     framecopy = np.copy(frame)
