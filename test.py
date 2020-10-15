bcf7078a662fc80b27a0787f03ca7286503b5709
sszxc <im.zhangxc@foxmail.com>
2020-10-15 15:20:07 +0800

    标定板

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     print("Python.version: "+sys.version)
     print("OpenCV.version: " + cv2.__version__)
     
-    while(True):
-        path = "test_img"
-        random_filename = random.choice([x for x in os.listdir(path)])
-        print(path + "/" + random_filename)
-        img_raw = cv2.imread(path + "/" + random_filename)
+    # while(True):
+    #     path = "test_img"
+    #     random_filename = random.choice([x for x in os.listdir(path)])
+    #     print(path + "/" + random_filename)
+    #     img_raw = cv2.imread(path + "/" + random_filename)
 
-        cv2.imshow('frame', img_raw)
-        cv2.waitKey(1000)
+    #     cv2.imshow('frame', img_raw)
+    #     cv2.waitKey(1000)
